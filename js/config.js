19fcda4f6a95795310215701c5451d16cd06eae1
GonzaloCarrasco <goncarrass@gmail.com>
2026-05-18 00:30:23 +0200

    claves cambiadas

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ const CONFIG = {
     // SUPABASE CONFIGURATION
     // ============================================
     supabase: {
-        url: 'https://clyllexmxtwgjqgaswlq.supabase.co/rest/v1/',           // URL del proyecto
+        url: 'https://clyllexmxtwgjqgaswlq.supabase.co',           // URL del proyecto
         anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNseWxsZXhteHR3Z2pxZ2Fzd2xxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDY2MDcsImV4cCI6MjA5NDYyMjYwN30.imDCURe96RChmHnRSVkz6mTdyC8BcdMLkyg2dokFfys',  // Clave pública (anon key)
         // ⚠️ NUNCA expongas la service_role key en el frontend
     },
